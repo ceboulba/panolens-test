678cea4fdd6eaa10ad243729561cc1995119e0b3
ceboulba <antoine@archipicture.fr>
2020-03-17 14:03:51 +0100

    1 file modified

--- index.js
+++ index.js
@@ -9,3 +9,4 @@ import './style.css';
 const panorama = new PANOLENS.ImagePanorama( 'https://res.cloudinary.com/archipicture/image/upload/v1557515706/ca_pano.jpg' );
 const viewer = new PANOLENS.Viewer();
 viewer.add( panorama );
+viewer.controlBar = false;
