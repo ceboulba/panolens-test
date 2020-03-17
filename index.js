8b97045bd9a9c2ccbcd2c21d1e0a8aeec76309a1
ceboulba <antoine@archipicture.fr>
2020-03-17 13:44:05 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import * as PANOLENS from 'panolens'
+import * as THREE from 'three'
+
+
+import './style.css';
+
+
+
+const panorama = new PANOLENS.ImagePanorama( 'https://res.cloudinary.com/archipicture/image/upload/v1557515706/ca_pano.jpg' );
+const viewer = new PANOLENS.Viewer();
+viewer.add( panorama );
